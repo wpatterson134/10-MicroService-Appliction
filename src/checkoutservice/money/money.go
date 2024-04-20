61b5625122bf3be36e252bc5b3e750bcd86502e8
Will Patterson <willpatterson@Wills-MacBook-Pro.local>
2024-04-19 21:33:17 -0700

    Test commit for checkoutservice

--- src/checkoutservice/money/money.go
+++ src/checkoutservice/money/money.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// making an edit to test Jenkins pipeline
+
 package money
 
 import (
